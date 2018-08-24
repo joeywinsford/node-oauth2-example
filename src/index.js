2a53d9379c0ac3c201af5fd2585a51324ccca969
Joe Lloyd <josephglloyd@gmail.com>
2018-08-24 07:58:38 +0100

    Add endpoint that echoes body data back

--- src/index.js
+++ src/index.js
@@ -1,8 +1,12 @@
-const createExpressApp = require('express')
+const express = require('express')
 
-const app = createExpressApp()
+const app = express()
 const PORT_NUMBER = 8091
 
+app.use(express.json())
+
 app.get('/', (_, res) => res.status(200).send(new Date().toUTCString()))
+
+app.post('/', (req, res) => res.status(200).send({ echo: req.body }))
 
 app.listen(PORT_NUMBER, () => console.log(`node-authentication example listening on port ${PORT_NUMBER}`))
